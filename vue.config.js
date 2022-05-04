0b4d80479985177653860b7cc72daaa5fe87273e
ukeming@icloud.com <ukeming@icloud.com>
2022-05-04 10:52:58 +0800

    change to http

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer:{
-    proxy:"http://143.198.65.33:9001",
+    disableHostCheck: true
   }
 })
